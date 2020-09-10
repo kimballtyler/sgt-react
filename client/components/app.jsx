8acec3cb7a913cd5dd39932cfb86188973d88080
kimballtyler <kimballstyler@gmail.com>
2020-09-09 21:29:35 -0400

    Completed user can view average grade

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,21 +5,34 @@ import GradeTable from './grade-table';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.getAverageGrade = this.getAverageGrade.bind(this);
     this.state = {
       grades: []
     };
   }
 
+  getAverageGrade() {
+    const grades = this.state.grades.slice();
+    const totalGrades = grades.reduce((acc, cur) => {
+      return acc + cur.grade;
+    }, 0);
+    if (totalGrades === 0) {
+      return 0;
+    }
+    return Math.ceil(totalGrades / grades.length);
+  }
+
   componentDidMount() {
     fetch('/api/grades')
       .then(response => response.json())
-      .then(data => this.setState({ grades: data }));
+      .then(data => this.setState(state => ({ grades: data })));
   }
 
   render() {
+    this.getAverageGrade();
     return (
-      <div className="ml-4">
-        <Header />
+      <div className="ml-4 container">
+        <Header averageGrade={this.getAverageGrade()} />
         <GradeTable grades={this.state.grades} />
       </div>
     );
--- client/components/heading.jsx
+++ client/components/heading.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
   return (
-    <h2 className="mb-4">Student Grade Table</h2>
+    <div className="row mt-2">
+      <h2 className="mb-4 col-4">Student Grade Table</h2>
+      <div className="col-2 display-5 text-muted offset-lg-6 mt-1">Average Grade <span className="badge-pill badge badge-info">{props.averageGrade}</span>
+      </div>
+    </div>
   );
 }
 
